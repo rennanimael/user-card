69fd4df032dcf430127f3190b59507e5eb315ae3
laboratorio <imael.rennan@ufms.br>
2023-04-04 21:46:05 -0400

    atualizando

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './App.css'
+import Card from './components/Card';
 
 function App() {
   
@@ -167,58 +168,11 @@ function App() {
   ]);
   return (
     <div className="App">
-      {/*{user.map(usuario =>*/}
-      {users.map(user => (<div className='profile-card'>
-      <div className="image">
-        <div className="profile-img">
-          <span className="initials">{user.iniciais}</span>
-        </div>
-      </div>
-      <div className="text-data">
-        <span className="name">{user.nome}</span>
-        <span className="job">{user.profissao}</span>
-      </div>
-      <div className="text-data">
-        <span className="cidade">{user.cidade}/{user.estado}</span>
-        <span className="aniversario">Aniversário: {user.nascimento}</span>
-      </div>
-      <div className="media-buttons">
-        <a href="#" style={{ background: '#4267b2' }} className="link">
-          <i className="bx bxl-facebook"></i>
-        </a>
-        <a href="#" style={{ background: '#1da1f2' }} className="link">
-          <i className="bx bxl-twitter"></i>
-        </a>
-        <a href="#" style={{ background: '#e1306c' }} className="link">
-          <i className="bx bxl-instagram"></i>
-        </a>
- 
-        <a href="#" style={{ background: '#ff0000' }} className="link">
-          <i className="bx bxl-youtube"></i>
-        </a>
-      </div>
-      <div className="buttons">
-        <button className="button">Altura {user.altura}</button>
-        <button className="button">Peso {user.peso}kg</button>
-        <button className="button">IMC {user.imc}</button>
-      </div>
-      <div className="analytics">
-        <div className="data">
-          <i className="bx bx-heart"></i>
-          <span className="nu">{user.likes}k</span>
-        </div>
-        <div className="data">
-          <i className="bx bx-message-rounded"></i>
-          <span className="nu">{user.messages}k</span>
-        </div>
-        <div className="data">
-          <i className="bx bx-share"></i>
-          <span className="nu">{user.shares}k</span>
-        </div>
-      </div>
-    </div>))}   
-    </div>
-  )
+        {users.map(user => (<Card user={user}/>))}
+
+
+        </div>)
 }
+
 
 export default App
---
+++ src/components/Card/index.tsx
@@ -0,0 +1,53 @@
+import './styles.css'
+
+export default function Card({ user }){
+  return (<div className='profile-card'>
+      <div className="image">
+        <div className="profile-img">
+          <span className="initials">{user.iniciais}</span>
+        </div>
+      </div>
+      <div className="text-data">
+        <span className="name">{user.nome}</span>
+        <span className="job">{user.profissao}</span>
+      </div>
+      <div className="text-data">
+        <span className="cidade">{user.cidade}/{user.estado}</span>
+        <span className="aniversario">Aniversário: {user.nascimento}</span>
+      </div>
+      <div className="media-buttons">
+        <a href="#" style={{ background: '#4267b2' }} className="link">
+          <i className="bx bxl-facebook"></i>
+        </a>
+        <a href="#" style={{ background: '#1da1f2' }} className="link">
+          <i className="bx bxl-twitter"></i>
+        </a>
+        <a href="#" style={{ background: '#e1306c' }} className="link">
+          <i className="bx bxl-instagram"></i>
+        </a>
+ 
+        <a href="#" style={{ background: '#ff0000' }} className="link">
+          <i className="bx bxl-youtube"></i>
+        </a>
+      </div>
+      <div className="buttons">
+        <button className="button">Altura {user.altura}</button>
+        <button className="button">Peso {user.peso}kg</button>
+        <button className="button">IMC {user.imc}</button>
+      </div>
+      <div className="analytics">
+        <div className="data">
+          <i className="bx bx-heart"></i>
+          <span className="nu">{user.likes}k</span>
+        </div>
+        <div className="data">
+          <i className="bx bx-message-rounded"></i>
+          <span className="nu">{user.messages}k</span>
+        </div>
+        <div className="data">
+          <i className="bx bx-share"></i>
+          <span className="nu">{user.shares}k</span>
+        </div>
+      </div>
+    </div>)       
+}
